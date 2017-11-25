c0356ef2ed78c3dad40b854083cf5b11e757c059
Mike Salisbury <michael.salisbury87@gmail.com>
2017-11-24 17:58:11 -0800

    fix bug in Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,13 +1,13 @@
 class Event < ApplicationRecord
+  CATEGORIES = %w(Music Food&Drink Classes Arts Parties
+    Sports&Wellness Networking)
+    
   validates :title, :description, :location, :start_datetime,
    :end_datetime, :img_url, :category, :privacy, presence: true
   validates :privacy, inclusion: { in: ['private, public'], message:
     "%{value} is not a valid privacy filter"}
   validates :category, inclusion: { in: CATEGORIES, message:
     "%{value} is not a valid category" }
-
-  CATEGORIES = %w(Music Food&Drink Classes Arts Parties
-    Sports&Wellness Networking)
 
 
   belongs_to :organizer,
